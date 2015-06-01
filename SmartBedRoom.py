03945eb2488499141c05956dc91062598f1bf3cb
kevin_lo <owl3808@gmail.com>
2015-06-02 00:44:28 +0800

    sample alarm clock

--- SmartBedRoom.py
+++ SmartBedRoom.py
@@ -1,14 +1,38 @@
-import light, lightSensor
+import light, lightSensor, clock, musicPlayer, button
 import RPi.GPIO as GPIO
-
 GPIO.setwarnings(False)
+
+enableAlarm = True
+isplayingmusic = False
 
 lightsensor = lightSensor.lightSensor(7)
 ledlight = light.light(3)
+alarmclock = clock.Clock()
+button = button.Button(12)
+musicPlayer = musicPlayer.MusicPlayer('little_love_song.mp3')
+
+alarmclock.settingAlarmClock(0,23)
 
 while True:
 	if lightsensor.isLightful():
 		ledlight.lightOff()
 	else:
 		ledlight.lightOn()
-		
+	
+	if enableAlarm:
+		if alarmclock.isAchiveAlarmTime():
+			if not isplayingmusic:
+				print 'alarmclock: alarming!'
+				musicPlayer.play()
+				isplayingmusic = True
+
+	if enableAlarm and button.isPressed():
+		musicPlayer.stop()
+		ismucicplaying = False
+		enableAlarm=False
+
+	if not enableAlarm and button.isPressed():
+		alarmclock.settingAlarmClockToNextDay()
+		enableAlarm=True
+
+
