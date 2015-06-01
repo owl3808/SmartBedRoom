4bcfc83e9d11291a90af8950e874645e60cb0e4e
kevin_lo <owl3808@gmail.com>
2015-06-01 16:46:25 +0800

    motor as class and in background

--- motor.py
+++ motor.py
@@ -1,69 +1,80 @@
 # Import required libraries
 import sys
 import time
+from threading import Thread
 import RPi.GPIO as GPIO
- 
-# Use BCM GPIO references
-# instead of physical pin numbers
-#GPIO.setmode(GPIO.BCM)
-GPIO.setmode(GPIO.BOARD)
- 
-# Define GPIO signals to use
-# Physical pins 11,15,16,18
-# GPIO17,GPIO22,GPIO23,GPIO24
-StepPins = [13,15,16,18]
- 
-# Set all pins as output
-for pin in StepPins:
-  print "Setup pins"
-  GPIO.setup(pin,GPIO.OUT)
-  GPIO.output(pin, False)
- 
-# Define advanced sequence
-# as shown in manufacturers datasheet
-Seq = [[1,0,0,0],
-       [1,1,0,0],
-       [0,1,0,0],
-       [0,1,1,0],
-       [0,0,1,0],
-       [0,0,1,1],
-       [0,0,0,1],
-       [1,0,0,1]]
-        
-StepCount = len(Seq)-1
-StepDir = 1 # Set to 1 or 2 for clockwise
-            # Set to -1 or -2 for anti-clockwise
- 
-# Read wait time from command line
-if len(sys.argv)>1:
-  WaitTime = int(sys.argv[1])/float(1000)
-else:
-  WaitTime = 10/float(1000)
- 
-# Initialise variables
-StepCounter = 0
- 
-# Start main loop
-while True:
- 
-  for pin in range(0, 4):
-    xpin = StepPins[pin]
-    #print StepCounter
-    #print pin
-    if Seq[StepCounter][pin]!=0:
-      print " Step %i Enable %i" %(StepCounter,xpin)
-      GPIO.output(xpin, True)
-    else:
-      GPIO.output(xpin, False)
- 
-  StepCounter += StepDir
- 
-  # If we reach the end of the sequence
-  # start again
-  if (StepCounter>StepCount):
-    StepCounter = 0
-  if (StepCounter<0):
-    StepCounter = StepCount
- 
-  # Wait before moving on
-  time.sleep(WaitTime)
+
+class Stepper:
+    def __init__(self): 
+        # Using physical pin numbers
+        GPIO.setmode(GPIO.BOARD)
+         
+        # Define GPIO signals to use
+        self.StepPins = [13,15,16,18]
+         
+        # Set all pins as output
+        for pin in self.StepPins:
+            print "Setup pins"
+            GPIO.setup(pin,GPIO.OUT)
+            GPIO.output(pin, False)
+         
+        # Define advanced sequence
+        # as shown in manufacturers datasheet
+        self.Seq = [[1,0,0,0],
+               [1,1,0,0],
+               [0,1,0,0],
+               [0,1,1,0],
+               [0,0,1,0],
+               [0,0,1,1],
+               [0,0,0,1],
+               [1,0,0,1]]
+                
+        self.StepCount = len(self.Seq)-1
+        self.StepDir = 1 # Set to 1 or 2 for clockwise
+                    # Set to -1 or -2 for anti-clockwise
+        self.WaitTime = 10/float(1000)
+
+    def setWaitTime(self, waittime):
+        self.WaitTime = float(float(waittime)/1000)
+        print "set waittime: %f" % self.WaitTime
+
+    def setStepDir(self, stepdir):
+        if stepdir < 2 and stepdir > -2 and stepdir != 0:
+            self.StepDir = stepdir
+
+    def run(self, round=0, no=0):
+        # Initialise variables
+        StepCounter = 0
+        roundCounter = 0
+
+        # Start main loop
+        while True:
+            for pin in range(0, 4):
+                xpin = self.StepPins[pin]
+                if self.Seq[StepCounter][pin]!=0:
+                    #print " Step %i Enable %i" %(StepCounter,xpin)
+                    GPIO.output(xpin, True)
+                else:
+                    GPIO.output(xpin, False)
+           
+            StepCounter += self.StepDir
+           
+            # If we reach the end of the sequence
+            # start again and count round number
+            if (StepCounter>self.StepCount):
+                StepCounter = 0
+                roundCounter += 1
+            if (StepCounter<0):
+                StepCounter = self.StepCount
+                roundCounter += 1
+
+            # if reach the setting round
+            if round!=0 and roundCounter >= round:
+		break
+           
+            # Wait before moving on
+            time.sleep(self.WaitTime)
+
+    def runInBackGound(self, round=0):
+        self.thread = Thread(target=self.run, args=(round,0))
+        self.thread.start()
