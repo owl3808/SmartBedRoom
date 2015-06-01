56cfba63b4fa6d9c0716af24148eed447057c5d8
kevin_lo <owl3808@gmail.com>
2015-06-01 12:59:26 +0800

    play mp3 in background

--- musicPlayer.py
+++ musicPlayer.py
@@ -6,9 +6,11 @@ class MusicPlayer:
 
 	def play(self):
 		mixer.music.play()
-		while mixer.music.get_busy() == True:
-			continue
+		#while mixer.music.get_busy() == True:
+		#	continue
 
 if __name__ == '__main__':
 	mp3player = MusicPlayer('little_love_song.mp3')
 	mp3player.play()
+	import time
+	time.sleep(10)
