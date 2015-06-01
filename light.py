e9802f1014e046bed27edadbcb9f3dea57a2a2ac
kevin_lo <owl3808@gmail.com>
2015-06-01 13:00:34 +0800

    LED change with status check

--- light.py
+++ light.py
@@ -1,15 +1,21 @@
 import RPi.GPIO as GPIO
 
 class light:
+	isLightOn=False
 	def __init__(self, pin):
 		self.lightpin = pin
 		GPIO.setup(self.lightpin, GPIO.OUT)
+		GPIO.output(self.lightpin, False)
 
 	def lightOn(self):
-		GPIO.output(self.lightpin, True)
+		if not self.isLightOn:
+			GPIO.output(self.lightpin, True)
+			self.isLightOn=True
 
 	def lightOff(self):
-		GPIO.output(self.lightpin, False)
+		if self.isLightOn:
+			GPIO.output(self.lightpin, False)
+			self.isLightOn=False
 
 if __name__ == '__main__':
 	import time
